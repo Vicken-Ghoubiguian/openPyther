3dc446c2d68b9c993a5d686263a6953f16d7f2cc
Eric GHOUBIGUIAN <ericghoubiguian@live.fr>
2021-04-28 09:08:36 +0200

    Definition of the python class named OpenWeatherPy

--- openWeatherPy.py
+++ openWeatherPy.py
@@ -1,1 +1,74 @@
-print("Hello World !")
+# Definition of the OpenWeatherPy class...
+class OpenWeatherPy:
+
+	# Definition of all class attributes....
+
+	# Definition of the class constructor...
+	def __init__(self):
+
+		print("Init function...")
+
+	#
+	def getCoords(self):
+
+		print("getCoords function...")
+
+	#
+	def getWeather(self):
+
+		print("getWeather function...")
+
+	#
+	def getTemperature(self):
+
+		print("getTemperature function...")
+
+	#
+	def getFeelingLikeTemperature(self):
+
+		print("getFeelingLikeTemperature function...")
+
+	#
+	def getMinTemperature(self):
+
+		print("getMinTemperature function...")
+
+	#
+	def getMaxTemperature(self):
+
+		print("getMaxTemperature function...")
+
+	#
+	def getPressure(self):
+
+		print("getPressure function...")
+
+	#
+	def getHumidity(self):
+
+		print("getHumidity function...")
+
+	#
+	def getWind(self):
+
+		print("getWind function...")
+
+	#
+	def getSunrise(self):
+
+		print("getSunrise function...")
+
+	#
+	def getSunset(self):
+
+		print("getSunset function...")
+
+	#
+	def getGeographicLocation(self):
+
+		print("getGeographicLocation function...")
+
+	#
+	def getUltraViolet(self):
+
+		print("getUltraViolet function...")
