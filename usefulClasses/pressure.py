d7c9b093e361cb4bedc514ed7d29d71915d183fa
Eric GHOUBIGUIAN <ericghoubiguian@live.fr>
2021-04-28 12:12:54 +0200

    Tell me some things

---
+++ usefulClasses/pressure.py
@@ -0,0 +1,56 @@
+#
+from usefulEnums import PressureEnum
+
+# Definition of the Pressure class...
+class Pressure:
+
+	#
+	def __init__(self, value):
+
+		self.__value = value
+		self.__measureUnit = PressureEnum.PressureEnum.HECTOPASCAL
+		self.__measureUnitSymbol = "hPa"
+
+	#
+	def setPressureAsHectoPascal(self):
+
+		print("Conversion in HectoPascal...")
+
+	#
+	def setPressureAsPascal(self):
+
+		print("Conversion in Pascal...")
+	#
+	def setPressureAsBar(self):
+
+		print("Conversion in Bar...")
+
+	#
+	def setPressureAsAtmosphere(self):
+
+		print("Conversion in Atmosphere...")
+
+	#
+	def setPressureAsTorr(self):
+
+		print("Conversion in Torr...")
+
+	#
+	def setPressureAsPoundsPerSquareInch(self):
+
+		print("Conversion in Pounds Per Square Inch...")
+
+	#
+	def getValue(self):
+
+		return self.__value
+
+	#
+	def getMeasureUnit(self):
+
+		return self.__measureUnit
+
+	#
+	def getSymbolUnit(self):
+
+		return self.__measureUnitSymbol		
--- usefulClasses/uv.py
+++ usefulClasses/uv.py
@@ -9,23 +9,23 @@ class UV:
 
 		if value <= 2:
 
-			determiedUVRisk = UVRiskEnum.UVRiskEnum.Low
+			determiedUVRisk = UVRiskEnum.UVRiskEnum.LOW
 
 		elif 3 <= value and value <= 5:
 
-			determiedUVRisk = UVRiskEnum.UVRiskEnum.Moderate
+			determiedUVRisk = UVRiskEnum.UVRiskEnum.MODERATE
 
 		elif 6 <= value and value <= 7:
 		
-			determiedUVRisk = UVRiskEnum.UVRiskEnum.High
+			determiedUVRisk = UVRiskEnum.UVRiskEnum.HIGH
 
 		elif 8 <= value && value <= 10:
 
-			determiedUVRisk = UVRiskEnum.UVRiskEnum.Very_High
+			determiedUVRisk = UVRiskEnum.UVRiskEnum.VERY_HIGH
 
 		else:
 
-			determiedUVRisk = UVRiskEnum.UVRiskEnum.Extreme
+			determiedUVRisk = UVRiskEnum.UVRiskEnum.EXTREME
 
 		self.__index = value
 		self.__risk = determiedUVRisk
