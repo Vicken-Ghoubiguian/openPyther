e8fc7fbece615660cd1022b341e2396bf6ed529d
Eric GHOUBIGUIAN <ericghoubiguian@live.fr>
2021-04-28 09:32:55 +0200

    Definition and modification of some things

---
+++ coordinates.py
@@ -0,0 +1,18 @@
+# Definition of the Coordinates class...
+class Coordinates:
+
+	# Definition of the Coordinates class constructor...
+	def __init__(self, longitude, latitude):
+
+		self.__longitude = longitude
+		self.__latitude = latitude
+
+	#
+	def getLongitude(self):
+
+		return self.__longitude
+
+	#
+	def getLatitude(self):
+
+		return self.__latitude
