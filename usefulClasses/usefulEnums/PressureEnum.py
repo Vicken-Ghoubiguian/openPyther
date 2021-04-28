85d6eca5e7e474cd6834dbd548554ba701c9ab6a
Eric GHOUBIGUIAN <ericghoubiguian@live.fr>
2021-04-28 11:12:51 +0200

    New enums

---
+++ usefulClasses/usefulEnums/PressureEnum.py
@@ -0,0 +1,12 @@
+# Importation of the Enum class to define the enum below...
+from enum import Enum
+
+# Definition of the Pressure enum...
+class Pressure(Enum):
+
+	HECTOPASCAL = 1
+	PASCAL = 2
+	BAR = 3
+	ATMOSPHERE = 4
+	TORR = 5
+	POUNDSPERSQUAREINCH = 6
---
+++ usefulClasses/usefulEnums/TemperatureEnum.py
@@ -0,0 +1,9 @@
+# Importation of the Enum class to define the enum below...
+from enum import Enum
+
+# Definition of the Temperature enum...
+class Temperature(Enum):
+
+	KELVIN = 1
+	CELSIUS = 2
+	FAHRENHEIT = 3
