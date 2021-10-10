d320945ff634dc16e05677ebf7979a76418abd75
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-10 09:44:15 +0200

    Sommy

---
+++ openPyther.py
@@ -0,0 +1,77 @@
+# Definition of the OpenWeatherPy class...
+class OpenWeatherPy:
+
+	# Definition of the first OpenWeatherPy class constructor...
+	def __init__(self, city, countryCode, APIKey):
+
+		print("Init function (1)...")
+
+	# Definition of the second OpenWeatherPy class constructor...
+	def __init__(self, city, APIKey):
+
+		print("Init function (2)...")	
+
+	#
+	def getCoords(self):
+
+		print("getCoords function...")
+
+	#
+	def getWeather(self):
+
+		print("getWeather function...")
+
+	#
+	def getTemperature(self):
+
+		print("getTemperature function...")
+
+	#
+	def getFeelingLikeTemperature(self):
+
+		print("getFeelingLikeTemperature function...")
+
+	#
+	def getMinTemperature(self):
+
+		print("getMinTemperature function...")
+
+	#
+	def getMaxTemperature(self):
+
+		print("getMaxTemperature function...")
+
+	#
+	def getPressure(self):
+
+		print("getPressure function...")
+
+	#
+	def getHumidity(self):
+
+		print("getHumidity function...")
+
+	#
+	def getWind(self):
+
+		print("getWind function...")
+
+	#
+	def getSunrise(self):
+
+		print("getSunrise function...")
+
+	#
+	def getSunset(self):
+
+		print("getSunset function...")
+
+	#
+	def getGeographicLocation(self):
+
+		print("getGeographicLocation function...")
+
+	#
+	def getUltraViolet(self):
+
+		print("getUltraViolet function...")
