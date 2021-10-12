7dec4f8ab5653b06a6bee1857d1b37b31fc4d01c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-12 10:30:54 +0200

    For package init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	readme_description = fh.read()
+
+setup(name='openPyther',
+version='0.1',
+description='Python package to get, treat and return weather from the OpenWeatherMap's API',
+long_description=readme_description,
+long_description_content_type="text/markdown",
+url='#',
+author='Wicken',
+author_email='ericghoubiguian@live.fr',
+license='MIT',
+packages=['openPyther'],
+install_requires=['requests'],
+zip_safe=False,
+python_requires='>=3.7.2')
