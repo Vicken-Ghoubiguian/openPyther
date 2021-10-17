19fd9938aeb0155ee650839437d35601cc1e7c31
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-17 20:51:50 +0200

    DF

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import openPyther.openPyther as openPyther
 import sys
 
 # Initialization of the 'openPyther' object with the wished localisation, the wished country code and the OpenWeather API key as parameters...
-openPytherObject = openPyther.OpenPyther("", "", "")
+openPytherObject = openPyther.OpenPyther(sys.argv[0], sys.argv[1], sys.argv[2])
 
 # Display the 'openPyther' object as a string...
 print(openPytherObject)
