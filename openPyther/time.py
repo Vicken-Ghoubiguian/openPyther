5e13ed0f4a433008a57e0d1ef147cc7e4b7d7113
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-20 07:12:21 +0200

    All getters for time

--- openPyther/time.py
+++ openPyther/time.py
@@ -16,6 +16,26 @@ class Time:
         self.__sunsetAsTimestampAccordingTheirTimezone = self.__sunsetAsTimestampAccordingToUtc + self.__utcOffsetAsTimestamp
 
     #
+    def getSunriseAsTimestampAccordingToUtc(self):
+
+        return self.__sunriseAsTimestampAccordingToUtc
+
+    #
+    def getSunsetAsTimestampAccordingToUtc(self):
+
+        return self.__sunsetAsTimestampAccordingToUtc
+
+    #
+    def getSunriseAsTimestampAccordingTheirTimezone(self):
+
+        return self.__sunriseAsTimestampAccordingTheirTimezone
+
+    #
+    def getSunsetAsTimestampAccordingTheirTimezone(self):
+
+        return self.__sunsetAsTimestampAccordingTheirTimezone
+
+    #
     def __str__(self):
         
         return ""
