e3a81961ed0f0ebfb903aa1da44272ea96ea0dd5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-21 13:19:47 +0200

    Comments in weather class

--- openPyther/weather.py
+++ openPyther/weather.py
@@ -9,22 +9,22 @@ class Weather:
 		self.__description = description
 		self.__iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
 
-	#
+	# Definition of the id's getter...
 	def getId(self):
 
 		return self.__id
 
-	#
+	# Definition of the main's getter...
 	def getMain(self):
 
 		return self.__main
 
-	#
+	# Definition of the description's getter...
 	def getDescription(self):
 
 		return self.__description
 
-	#
+	# Definition of the url of icon's getter...
 	def getIconUrl(self):
 
 		return self.__iconUrl
