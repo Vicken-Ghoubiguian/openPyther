f9332e8321bcec59712198414be68c1c15ed49f1
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-21 11:39:26 +0200

    Comments in GL

--- openPyther/geographicLocation.py
+++ openPyther/geographicLocation.py
@@ -8,22 +8,22 @@ class GeographicLocation:
 		self.__localisationName = localisationName
 		self.__utcOffsetAsTimestamp = utcOffsetAsTimestamp
 
-	#
+	# Definition of the country code's getter...
 	def getCountryCode(self):
 
 		return self.__countryCode
 
-	#
+	# Definition of the localisation's getter...
 	def getLocalisationName(self):
 
 		return self.__localisationName
 
-	#
+	# Definition of the UTC offset as timestamp's getter...
 	def getUtcOffsetAsTimestamp(self):
 
 		return self.__utcOffsetAsTimestamp
 
-	#
+	# Definition of the __str__ method to display the current object as a string...
 	def __str__(self):
 		
 		return "[{} ({})]".format(str(self.__localisationName), str(self.__countryCode))
