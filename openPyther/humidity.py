6c16f3b2c96f51e4e53c8f649d6dcc35ba902f35
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-21 13:18:16 +0200

    Comments in humidity class

--- openPyther/humidity.py
+++ openPyther/humidity.py
@@ -7,17 +7,17 @@ class Humidity:
 		self.__value = value
 		self.__unitOfMeasure = "%"
 
-	#
+	# Definition of the value's getter...
 	def getValue(self):
 
 		return self.__value
 
-	#
+	# Definition of the unit of measure's getter...
 	def getUnitOfMeasure(self):
 
 		return self.__unitOfMeasure
 
 	# Definition of the __str__ method to display the current object as a string...
 	def __str__(self):
-		
+
 		return "{} {}".format(str(self.__value), str(self.__unitOfMeasure))
